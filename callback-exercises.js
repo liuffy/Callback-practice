3a7cc8fca2d9bbeb65c5cf67d443c39598fa2f17
Sophia Liu <sliu3@wellesley.edu>
2016-10-25 20:21:10 -0700

    Clockity clock

--- callback-exercises.js
+++ callback-exercises.js
@@ -17,19 +17,55 @@
 class Clock {
   constructor() {
     // 1. Create a Date object.
+    const currentTime = new Date();
     // 2. Store the hours, minutes, and seconds.
+    this.hours = currentTime.getHours();
+    this.minutes = currentTime.getMinutes();
+    this.seconds = currentTime.getSeconds();
     // 3. Call printTime.
+
+    this.printTime();
     // 4. Schedule the tick at 1 second intervals.
+
+    // SET intervals
+    // EX: setInterval(function(){ alert("Hello"); }, 3000);
+    // does function every X millliseconds
+    setInterval(this._tick.bind(this), 1000); // bind...what are you?
   }
 
   printTime() {
     // Format the time in HH:MM:SS
+    const timeString = [this.hours, this.minutes, this.seconds].join(":");
     // Use console.log to print it.
+    console.log(timeString);
   }
 
   _tick() {
     // 1. Increment the time by one second.
+    this._incrementSeconds();
     // 2. Call printTime.
+    this.printTime();
+  }
+
+  _incrementSeconds(){
+
+    this.seconds += 1
+    if (this.seconds === 60){
+      this.seconds = 0;
+      this._incrementMinutes();
+    }
+  }
+
+  _incrementMinutes(){
+    this.minutes += 1
+    if (this.minutes === 60){
+      this.minutes = 0;
+      this._incrementHours();
+    }
+  }
+
+  _incrementHours(){
+    this.hours = (this.hours + 1) % 24; // smart way to loop time
   }
 }
 
